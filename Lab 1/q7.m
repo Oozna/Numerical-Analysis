37f20a0979e877b1911c5e67d264933ed7e75913
Eric Johansson <ericjohansson@Erics-MacBook-Pro.local>
2022-08-29 14:27:44 +0200

    fixed question 7

--- Lab 1/q7.m
+++ Lab 1/q7.m
@@ -2,3 +2,22 @@ format long
 clear
 clc
 
+SecantMethod(-1.4,-1.3)
+SecantMethod(1.8,2)
+SecantMethod(3.7,3.9)
+
+function SecantMethod(left, right)
+    iteration = 0;
+
+    while abs(left-right) > 10^-15
+        x2=(left*F(right)-right*F(left))/(F(right)-F(left));
+        left=right;        
+        right=x2;
+        iteration = iteration + 1;
+        fprintf("Iteration %i: \t %.15f\n", iteration, x2);
+    end
+end
+
+function ret = F(x)
+    ret = -(cos(x)+x/5);
+end
