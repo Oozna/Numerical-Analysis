95dfb365cb51ae1287ea3ddf2270ef23e7abffa8
Eric Johansson <johanssonericwilliam@gmail.com>
2022-10-10 18:19:49 +0200

    lab 4 done

--- Lab 4/q1.m
+++ Lab 4/q1.m
@@ -1,34 +1,93 @@
 header
+% Made by Eric Johansson, Can Kupeli and Samuel Greenberg
 
 % Objective function
 % Z = max(51.5*w +119.5*b);
-%-b <= 0
-%-w <= 0
-%b <= 39
-%w <= 108
-%b+w <= 110
-%w*110.5+b*198.5 <= 12000
-%w*11+b*32 <= 1250
+
+% Constraints
+%              -b <= 0
+%              -w <= 0
+%               b <= 39
+%               w <= 108
+%             b+w <= 110
+% w*110.5+b*198.5 <= 12000
+%       w*11+b*32 <= 1250
+
+
+
+syms t
+y1 = vpa(solve(f1(t) == f2(t)));
+x1 = vpa(f1(y1));
+
+y2 = vpa(solve(f1(t) == 0));
+x2 = vpa(f1(y2));
+
+x3 = vpa(solve((1250/11) - (32/11)*t == 0));
+y3 = vpa(1250/11 - 32/11*x3);
+
+fprintf("Point 1: x=%.3f   y=%.3f \n",x1,y1)
+fprintf("Point 2: x=%.3f   y=%.3f \n",x2,y2)
+fprintf("Point 3: x=%.3f   y=%.3f \n",x3,y3)
+
+
+plot(x1, y1, 'ok', x2, y2, 'ok', x3, y3, 'ok')
 
 hold on
 axis([0 120 0 120]);
-x = 0:1:200;
-y11 = 110 - x;
-y12 = (12000-198.5*x)/110.5;
-y13 = (1250-32*x)/11;
-%plot(x,y11,'r',x,y12,'b',x,y13,"k");
+x = 0:0.1:200;
+y22 = 110 - x;
+y33 = (12000-198.5*x)/110.5;
+y44 = (1250-32*x)/11;
+plot(x,y22,'r',x,y33,'b',x,y44,"k");
 
 
 
-%w*110.5+b*198.5 + (b+w > 50)(2*(b+w-50))<= 12000
-%
-%w*110.5+b*198.5 <= 12000
+
+[b w] = meshgrid(x);
+
+cond1 = -b <= 0;
+cond2 = -w <= 0;
+cond3 = w*11+b*32 <= 1250; 
+cond4 = w*110.5+b*198.5 <= 12000;
+cond5 = b+w <= 110;
+
+cond1 = double(cond1);
+cond2 = double(cond2);
+cond3 = double(cond3);
+cond4 = double(cond4);
+cond5 = double(cond5);
+
+cond1(cond1 == 0) = NaN;
+cond2(cond2 == 0) = NaN; 
+cond3(cond3 == 0) = NaN; 
+cond4(cond4 == 0) = NaN; 
+cond5(cond5 == 0) = NaN; 
+
+cond = cond1.*cond2.*cond3.*cond4.*cond5;
+surf(b,w,cond, 'EdgeColor','blue')
+view(0,90)
+hold off
 
 
-x1 = 0:1:50;
-x2 = 51:1:100;
-y21 = 110 - x;
-y33 = (1250-32*x)/11;
-y22 = (12000-198.5*x1)/110.5;
-ytest = ((-1105-3*x)-((1105+3*x).^2-4>(1985+2*x.^2-119500)).^(1/2))/2;
-plot(x,ytest,'k')
+[maxp,maxx,maxy] = max_profit([[x1,y1];[x2,y2];[x3,y3]]);
+fprintf("Max profit is %f at x = %f and y = %f \n",maxp,maxx,maxy)
+
+function [maxp,maxx,maxy] = max_profit(arr)
+    maxp = 0;
+    for i = 1:length(arr)
+        profit = 51.50*arr(i,2)+119.50*arr(i,1);
+        if (profit > maxp)
+            maxp = profit;
+            maxx = arr(i,1);
+            maxy = arr(i,2);
+        end
+    end
+end
+
+function y = f1(x)
+y = (12000/198.5) - (110.5/198.5)*x;
+end
+
+function y = f2(x)
+y = (1250/32) - (11/32)*x;
+end
---
+++ Lab 4/q4.m
@@ -0,0 +1,24 @@
+header
+% Made by Eric Johansson, Can Kupeli and Samuel Greenberg
+
+
+% Objective function
+%max(51.5x + 119.5y - 2o)
+
+%Constraints
+%x + y - o <= 50
+%110.5x + 198.5y <= 12000
+%11x + 32y <= 1250
+%o <= 60
+
+
+A = [110.5 198.5 0; 11 32 0; 1 1 -1; 0 0 1];
+b = [12000; 1250; 50; 60];
+f = [-51.5 -119.5 2];
+
+x = linprog(f, A, b);
+
+profit = 51.5*x(1) + 119.5*x(2) - 2*x(3);
+
+fprintf("Maximal profit is %.5f \n",profit)
+fprintf("This is when you have %.5f wheat and %.5f barley\n",x(1),x(2))
